1e3aa8fb578d6f7202ca0696d51ccd272c223a0c
Rokia_yasser <127583036+rokia-yassser@users.noreply.github.com>
2025-03-25 23:48:40 +0200

    Update Yolos2.py

--- Yolos2.py
+++ Yolos2.py
@@ -5,21 +5,15 @@ import cvzone
 import pandas as pd
 import time
 
-# Load model and move it to DirectML device
+
 dml_device = torch_directml.device()
 model = YOLO(r"C:\Users\HP\Desktop\ML-DL-basics\safety_project\ConstructionSafety\best.pt")
 model.model = model.model.to(dml_device)
 
-# Define class names
 classNames = ['boots', 'gloves', 'helmet', 'helmet on', 'no boots', 'no glove', 'no helmet', 'no vest', 'person', 'vest']
-
-# Open video capture
 cap = cv2.VideoCapture(r"C:\Users\HP\Desktop\ML-DL-basics\safety_project\ConstructionSafety\8293012-hd_1920_1080_30fps.mp4")
-
-# Create DataFrame for detections
 df = pd.DataFrame(columns=['Class', 'Confidence', 'X1', 'Y1', 'X2', 'Y2'])
 
-# FPS tracking
 prev_frame_time = 0
 new_frame_time = 0
 
@@ -31,7 +25,7 @@ while True:
         print("Error reading frame from video stream.")
         break
 
-    # Perform object detection
+    
     results = model(img, stream=True)
     
     for r in results:
@@ -51,24 +45,24 @@ while True:
             except IndexError:
                 print("Error: Index out of range. Skipping detection.")
     
-    # Calculate FPS
+
     fps = 1 / (new_frame_time - prev_frame_time)
     prev_frame_time = new_frame_time
     cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
-    # Display frame
+    
     resized_img = cv2.resize(img, (800, 600))
     cv2.imshow("Image", resized_img)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Save detections to Excel
+
 try:
     df.to_excel('detections2.xlsx', index=False)
 except ModuleNotFoundError:
     print("Error: 'openpyxl' module not found. Install it using 'pip install openpyxl'.")
 
-# Release resources
+
 cap.release()
 cv2.destroyAllWindows()
